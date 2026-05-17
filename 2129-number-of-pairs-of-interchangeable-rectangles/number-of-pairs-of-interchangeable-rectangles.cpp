1c39e0aefda3de42b688d36ad4164641d6e91872
Akshat Chaudhary <chaudharyakshat555@gmail.com>
2026-05-17 17:24:08 +0530

    Time: 150 ms (29.77%) | Memory: 162.9 MB (6.02%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
-        // vector<double> divs;
         unordered_map<double,int> mp;
         for(auto it: rectangles){
             int num = it[0];
             int deno = it[1];
-            // divs.push_back((num*1.0)/deno);
             double div = (num*1.0)/deno;
             mp[div]++;
         }
-        // for(auto it: divs){
-        //     mp[it]++;
-        // }
         long long res =  0;
         for(auto it: mp){
             int nn = it.second;
